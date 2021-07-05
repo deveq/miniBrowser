e5e829920241ad1303fda79664c518ca9eb77582
deveq <deveq@kakao.com>
2021-07-05 23:48:59 +0900

    스토리보드 작성, 아울렛 연결

--- MiniBrowser/ViewController.h
+++ MiniBrowser/ViewController.h
@@ -6,8 +6,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UISegmentedControl *bookmarkSegmentedControl;
+@property (weak, nonatomic) IBOutlet UITextField *urlTextField;
+
+// WKWebView의 경우 WebKit/WebKit.h를 import해야함.
+@property (weak, nonatomic) IBOutlet WKWebView *mainWebView;
 
 
 @end
